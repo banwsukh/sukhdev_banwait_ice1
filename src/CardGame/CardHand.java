de6b9a1f7b98022ede9891face459751df55f6d8
banwsukh <ssbanwait78@gmail.com>
2020-01-29 19:02:00 -0500

    first commit

---
+++ src/CardGame/CardHand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+public class CardHand {
+
+        private int handSize = 52;
+	public Card[] cards = new Card[handSize];
+
+	
+        /**
+         * Add comments during class to explain what this method does.
+         */
+        public void generateHand()
+        {
+                int countCards = 0;
+		for(Card.Suit s: Card.Suit.values())
+                {
+                    for(Card.Value v: Card.Value.values())
+                    {
+                        cards[countCards] = (new Card(s,v));
+                        countCards++;
+                    }
+                }//end outter for
+        }//end method
+
+}
