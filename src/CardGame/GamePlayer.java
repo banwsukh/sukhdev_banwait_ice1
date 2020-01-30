56af140abd69f6de5f723d221d67e07ff5ce87df
banwsukh <ssbanwait78@gmail.com>
2020-01-29 19:04:50 -0500

    first commit

---
+++ src/CardGame/GamePlayer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+public class GamePlayer {
+
+	public static void main(String[] args) {
+		CardHand ch = new CardHand();
+                ch.generateHand();
+                for(Card c: ch.cards)
+                {
+                    System.out.println(c.getValue() + " of " + c.getSuit());
+                }
+               // System.out.println(ch.cards); show what happens when there is no toString()
+	}
+
+}
